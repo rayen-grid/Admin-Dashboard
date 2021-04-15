369a70b7247a729d3ed018afb7d1e44767bb1df7
rayen-grid <rayen512@hotmail.com>
2021-04-15 01:46:19 +0100

    styling

--- src/app/table-item/table-item.component.ts
+++ src/app/table-item/table-item.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-table-item',
   templateUrl: './table-item.component.html',
   styleUrls: ['./table-item.component.css']
 })
 export class TableItemComponent implements OnInit {
+  elements: any = [
+    {first: '1111111'},
+    {first: '2222222'},
+    {first: '3333333'},
+    
+  ];
+  headElements = ['sdqt'];
 
+
+ 
+  
   constructor() { }
 
   ngOnInit(): void {
